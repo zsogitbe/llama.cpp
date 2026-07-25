720d7fa4097f76e5d0eade5a92c1df87c1faf9d9
Adrien Gallouët <angt@huggingface.co>
2026-07-25 18:16:29 +0200

    vendor : update cpp-httplib to 0.51.0 (#26067)
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- scripts/sync_vendor.py
+++ scripts/sync_vendor.py
@@ -5,7 +5,7 @@ import os
 import sys
 import subprocess
 
-HTTPLIB_VERSION = "refs/tags/v0.50.1"
+HTTPLIB_VERSION = "refs/tags/v0.51.0"
 
 vendor = {
     "https://github.com/nlohmann/json/releases/latest/download/json.hpp":     "vendor/nlohmann/json.hpp",
