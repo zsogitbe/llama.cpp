dee2a846b82f15d27f84a48fa387cb53e0d99c25
Georgi Gerganov <ggerganov@gmail.com>
2026-07-27 14:54:46 +0300

    ggml : adjust logic for offloading ops to weight's backend (#25832)
    * ggml : adjust logic for offloading ops to weight's backend
    
    * llama : dsv4 graph fixes

--- src/llama-context.cpp
+++ src/llama-context.cpp
@@ -2473,11 +2473,12 @@ llm_graph_cb llama_context::graph_get_cb() const {
             ggml_set_name(cur, name);
         }
 
-        // norm may be automatically assigned to the backend of the previous layer, increasing data transfer between backends
+        // - norm may be automatically assigned to the backend of the previous layer, increasing data transfer between backends
+        // - force the last op of the layer on the specified backend to avoid running it on the backend of the next layer due to scheduling
         // FIXME: fix in ggml_backend_sched
         const bool full_offload = model.n_gpu_layers() > model.hparams.n_layer_all;
         if (ubatch.n_tokens < 32 || full_offload) {
-            if (il != -1 && strcmp(name, "norm") == 0) {
+            if (il != -1 && (strcmp(name, "norm") == 0 || strcmp(name, "l_last") == 0)) {
                 const auto & dev_layer = model.dev_layer(il);
                 for (const auto & backend : backends) {
                     if (ggml_backend_get_device(backend.get()) == dev_layer) {
