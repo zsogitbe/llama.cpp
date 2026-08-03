0b14b87d7c20cb753b94b96854dd7b45306fc696
Xuan-Son Nguyen <son@huggingface.co>
2026-08-03 12:45:24 +0200

    server: add notice for upcoming default port change 8080 --> 9931 (#26508)
    * server: add notice for upcoming default port change 8080 --> 6631
    
    * add link to PR
    
    * correct to 9931

--- tools/server/server.cpp
+++ tools/server/server.cpp
@@ -486,6 +486,13 @@ int llama_server(common_params & params, int argc, char ** argv) {
 
     SRV_INF("listening on %s\n", ctx_http.listening_address.c_str());
 
+    // TODO: remove this in the future
+    // check the string to also handle the .sock case
+    if (string_ends_with(ctx_http.listening_address, ":8080")) {
+        SRV_WRN("%s", "NOTICE: server default port will be changed to :9931 in a future release\n");
+        SRV_WRN("%s", "        ref: https://github.com/ggml-org/llama.cpp/pull/26508\n");
+    }
+
     if (is_router_server) {
         if (!params.models_preset_hf.empty()) {
             SRV_WRN(      "NOTE: using preset.ini from HF repo '%s'\n", params.models_preset_hf.c_str());
