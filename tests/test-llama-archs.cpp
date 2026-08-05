360e1349f0009c5ad99d21e3c4546b707addc68a
Niklas Wenzel <dev@nikwen.de>
2026-08-05 17:58:34 +0200

    tests: re-enable MiniMax M3 in `test-llama-archs` (#26633)

--- tests/test-llama-archs.cpp
+++ tests/test-llama-archs.cpp
@@ -432,7 +432,7 @@ static bool arch_supported(const llm_arch arch) {
 
     // FIXME: these hit scheduler/view-backed-output issues with WebGPU on CI.
 #ifdef GGML_USE_WEBGPU
-    if (arch == LLM_ARCH_DEEPSEEK32 || arch == LLM_ARCH_GLM_DSA || arch == LLM_ARCH_MINIMAX_M3) {
+    if (arch == LLM_ARCH_DEEPSEEK32 || arch == LLM_ARCH_GLM_DSA) {
         return false;
     }
 #endif // GGML_USE_WEBGPU
