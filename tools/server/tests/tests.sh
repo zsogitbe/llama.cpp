b3df57286c336255fd02c2162b0b5fe3adc2965e
Georgi Gerganov <ggerganov@gmail.com>
2026-08-11 09:07:13 +0300

    tests : clean-up server test, use `tests.sh` in ci (#26886)
    * tests : remove fetch_server_test_models.py
    
    * ci : use tests.sh wrapper of pytest

--- tools/server/tests/tests.sh
+++ tools/server/tests/tests.sh
@@ -6,11 +6,6 @@ cd $SCRIPT_DIR
 
 set -eu
 
-if [[ "${SLOW_TESTS:-0}" == 1 ]]; then
-    # Slow tests for tool calls need quite a few models ahead of time to avoid timing out.
-    python $SCRIPT_DIR/../../../scripts/fetch_server_test_models.py
-fi
-
 if [ $# -lt 1 ]
 then
     if [[ "${SLOW_TESTS:-0}" == 1 ]]; then
